9036575c0e717126c6b195d461ce833c565f5d0b
AnnaSteinberg <annasteinb@gmail.com>
2025-08-11 21:39:59 +0300

    cw_9: morgan.

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,21 @@ import express from "express";
 import {PORT} from "./config/libConfig.ts";
 import {libRouter} from "./routes/libRouter.ts";
 import {errorHandler} from "./errorHandler/errorHandler.ts";
+import morgan from "morgan";
+import * as fs from "node:fs";
 
 export  const launchServer = () => {
     const app = express();
 
     app.listen(PORT, () => console.log(`Server runs at http://localhost:${PORT}`));
 
+    const logStream = fs.createWriteStream("./src/logs/access.log", { flags: "a" });
 
 //============Middleware===========
 
     app.use(express.json());
+    app.use(morgan("dev"));
+    app.use(morgan('combined', {stream: logStream}));
 
 //============Router===========
 
